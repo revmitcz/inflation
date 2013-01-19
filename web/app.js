d5a0b8d33bd7859cc1f2d4d92ff3a58927e25e14
Ian <typppo@gmail.com>
2013-01-19 13:52:42 -0800

    move sitemap into its own route

--- web/app.js
+++ web/app.js
@@ -1,45 +1,10 @@
 var express = require('express')
-  , routes = require('./routes')
   , convert = require('./routes/convert.js')
+  , sitemap = require('./routes/sitemap.js')
   , http = require('http')
   , path = require('path')
-  , sm = require('sitemap')
-  , compare = require('../lib/calc.js')
 
 var app = express();
-
-var sitemap_urls = [];
-for (var i=1900; i <= compare.max_year; i++) {
-  var jstart = i;
-  if (i < 1950) {
-    jstart = 1990;
-  }
-  for (var j=jstart; j <= compare.max_year; j++) {
-    if (i===j) continue;
-    var priority;
-    if (i > 1990 && j > 1990) {
-      priority = .8;
-    }
-    else if (i > 1970 && j > 1970) {
-      priority = .6;
-    }
-    else if (i > 1900 && j > 1900) {
-      priority = .4;
-    }
-    else {
-      priority = .2;
-    }
-    sitemap_urls.push({url: 'inflation-' + i + '-to-' + j, changefreq: 'yearly', priority: priority});
-    sitemap_urls.push({url: 'in-' + i + '-dollars', changefreq: 'yearly', priority: priority});
-    sitemap_urls.push({url: i + '-dollars-in-' + j + '-dollars', changefreq: 'yearly', priority: priority});
-  }
-}
-console.log(sitemap_urls.length, 'in sitemap');
-var sitemap = sm.createSitemap ({
-  hostname: 'http://www.in2013dollars.com/',
-  cacheTime: 600000,        // 600 sec - cache purge period
-  urls: sitemap_urls,
-});
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
@@ -65,12 +30,7 @@ app.get('/:year-dollars-in-:year2-dollars', convert.main);
 app.get('/:year-dollars-in-:year2', convert.main);
 app.get('/year-:year', convert.main);
 app.get('/:year/:year2', convert.main);
-app.get('/sitemap.xml', function(req, res) {
-  sitemap.toXML( function (xml) {
-      res.header('Content-Type', 'application/xml');
-      res.send( xml );
-  });
-});
+app.get('/sitemap.xml', sitemap.main);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
